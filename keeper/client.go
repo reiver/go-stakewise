01b982dbbdb002bc7678904ee5d748755725331f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-07-25 07:51:53 -0700

    keeper.Client

--- keeper/client.go
+++ keeper/client.go
@@ -9,7 +9,14 @@ type Client struct {
 	conclient.Client
 }
 
-func MakeClient(contract Contract, rpcurl string) Client {
+func MakeClient(keepercontract KeeperContract, rpcurl string) Client {
+	var contract = conclient.Contract{
+		ABI:             abi,
+		Address:         keepercontract.Address,
+		ChainID:         keepercontract.ChainID,
+		FromBlockNumber: keepercontract.FromBlockNumber,
+	}
+
 	return Client{
 		Client:conclient.MakeClient(contract, rpcurl),
 	}
